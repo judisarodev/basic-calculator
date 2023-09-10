6ac75333e57bd3355fb7ac2aaf680e1e06a7266b
judisaro <judisaro@hotmail.com>
2023-09-10 16:22:42 -0500

    remover useState si no es usado

--- src/components/ShowHistory.js
+++ src/components/ShowHistory.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { FragmentContext } from "../context/FragmentContext";
 import './../styles/menu.css';
 
--- src/modals/MyModal.js
+++ src/modals/MyModal.js
@@ -1,4 +1,4 @@
-import React, {useContext, useRef, useState} from "react";
+import React, {useContext, useRef} from "react";
 import '../styles/modal.css'
 import { FragmentContext } from "../context/FragmentContext";
 
